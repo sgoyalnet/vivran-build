8f7506fc652b1bededa6c6c4afdcd8e127de8fdd
Shubham Goyal <shubham.goyal@qlik.com>
2017-06-09 17:03:34 +0530

    0.0.0

---
+++ main.bundle.js
@@ -0,0 +1,329 @@
+webpackJsonp([1],{
+
+/***/ "../../../../../src async recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = "../../../../../src async recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app-routing.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__urlencodedecode_urlencodedecode_component__ = __webpack_require__("../../../../../src/app/urlencodedecode/urlencodedecode.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppRoutingModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var routes = [
+    { path: '', redirectTo: '/urlencodedecode', pathMatch: 'full' },
+    { path: 'urlencodedecode', component: __WEBPACK_IMPORTED_MODULE_2__urlencodedecode_urlencodedecode_component__["a" /* UrlEncodeDecodeComponent */] },
+    { path: 'base64', component: __WEBPACK_IMPORTED_MODULE_2__urlencodedecode_urlencodedecode_component__["a" /* UrlEncodeDecodeComponent */] },
+];
+var AppRoutingModule = (function () {
+    function AppRoutingModule() {
+    }
+    return AppRoutingModule;
+}());
+AppRoutingModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["b" /* NgModule */])({
+        imports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */].forRoot(routes)],
+        exports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["a" /* RouterModule */]]
+    })
+], AppRoutingModule);
+
+//# sourceMappingURL=app-routing.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".nav-tabs>li.active>a,\r\n.nav-tabs>li.active>a:focus,\r\n.nav-tabs>li.active>a:hover {\r\n  border-top-color: transparent;\r\n}\r\n\r\n.nav-tabs>li>a:active, .nav-tabs>li>a:focus, .nav-tabs>li>a:hover {\r\n  border-top-color: transparent;\r\n}\r\n\r\n.brand-viv {\r\n  border-right-width: 1px !important;\r\n  border-right-color: #ddd !important;\r\n}\r\n\r\n.header-bar {\r\n    height: 3px;\r\n    background-color: #0088ce;\r\n    margin-bottom: 1px;\r\n}\r\n.parent {\r\n  min-height:100%;\r\n  position: relative;\r\n}\r\nfooter {\r\n  padding-top: 10px;\r\n  position:absolute;\r\n  bottom:-20;\r\n  right:10px;\r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"parent\">\n\t<div class=\"header-bar\"></div>\n\t<ul class=\"nav nav-tabs\" id=\"headerTab\">\n\t\t<li><a class=\"brand-viv\"><b>VivRan</b></a></li>\n\t\t<li routerLinkActive=\"active\" class=\"hidden-xs\"><a routerLink=\"/urlencodedecode\">URL Encoder/Decoder</a></li>\n\t\t<li routerLinkActive=\"active\" class=\"hidden-xs\"><a routerLink=\"/base64\">Base64 Encoder/Decoder</a></li>\n\t\t<li class=\"dropdown visible-xs\">\n\t\t\t<a class=\"dropdown-toggle\" data-toggle=\"dropdown\">\n\t\t\t\tMenu <span class=\"caret\"></span>\n\t\t\t</a>\n\t\t\t<ul class=\"dropdown-menu\" role=\"menu\">\n\t\t\t\t<li routerLinkActive=\"active\"><a routerLink=\"/urlencodedecode\">URL Encoder/Decoder</a></li>\n\t\t\t\t<li routerLinkActive=\"active\"><a routerLink=\"/base64\">Base64 Encoder/Decoder</a></li>\n\t\t\t</ul>\n  \t\t</li>\n\t</ul>\n\t<div class=\"container-fluid\">\n\t\t<router-outlet></router-outlet>\n\t</div>\n\t<footer class=\"pull-right\">&copy; <a href=\"https://sgoyal.net\">sgoyal.net</a></footer>\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__urlencodedecode_urlencodedecode_module__ = __webpack_require__("../../../../../src/app/urlencodedecode/urlencodedecode.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_routing_module__ = __webpack_require__("../../../../../src/app/app-routing.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_3__app_routing_module__["a" /* AppRoutingModule */],
+            __WEBPACK_IMPORTED_MODULE_2__urlencodedecode_urlencodedecode_module__["a" /* UrlEncoDedecodeModule */]
+        ],
+        providers: [__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["b" /* Title */]],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/urlencodedecode/urlencodedecode.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return UrlEncodeDecodeComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var UrlEncodeDecodeComponent = (function () {
+    function UrlEncodeDecodeComponent(titleService) {
+        this.titleService = titleService;
+        this.model = { url: '', orginalUrl: '' };
+    }
+    ;
+    UrlEncodeDecodeComponent.prototype.ngOnInit = function () {
+        this.titleService.setTitle(this.titleService.getTitle() + ' URL Encoder/Decoder');
+    };
+    // events
+    UrlEncodeDecodeComponent.prototype.onTyping = function (event) {
+        this.model.url = event.target.value;
+    };
+    UrlEncodeDecodeComponent.prototype.encodeUrl = function () {
+        this.model.orginalUrl = this.model.orginalUrl == '' ? this.model.url : this.model.orginalUrl;
+        this.model.url = encodeURIComponent(this.model.url).replace(/'/g, "%27").replace(/"/g, "%22");
+    };
+    UrlEncodeDecodeComponent.prototype.decodeUrl = function () {
+        this.model.url = decodeURIComponent(this.model.url.replace(/\+/g, " "));
+    };
+    UrlEncodeDecodeComponent.prototype.reset = function () {
+        this.model.url = this.model.orginalUrl;
+        this.model.orginalUrl = '';
+    };
+    UrlEncodeDecodeComponent.prototype.clear = function () {
+        this.model.orginalUrl = '';
+        this.model.url = '';
+    };
+    Object.defineProperty(UrlEncodeDecodeComponent.prototype, "diagnostic", {
+        //debug
+        get: function () { return JSON.stringify(this.model); },
+        enumerable: true,
+        configurable: true
+    });
+    return UrlEncodeDecodeComponent;
+}());
+UrlEncodeDecodeComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+        selector: 'url-code',
+        template: __webpack_require__("../../../../../src/app/urlencodedecode/urlencodedecode.template.html"),
+        styles: [__webpack_require__("../../../../../src/app/urlencodedecode/urlencodedecode.style.css")]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__["b" /* Title */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser__["b" /* Title */]) === "function" && _a || Object])
+], UrlEncodeDecodeComponent);
+
+var _a;
+//# sourceMappingURL=urlencodedecode.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/urlencodedecode/urlencodedecode.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__urlencodedecode_component__ = __webpack_require__("../../../../../src/app/urlencodedecode/urlencodedecode.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return UrlEncoDedecodeModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+var UrlEncoDedecodeModule = (function () {
+    function UrlEncoDedecodeModule() {
+    }
+    return UrlEncoDedecodeModule;
+}());
+UrlEncoDedecodeModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_1__urlencodedecode_component__["a" /* UrlEncodeDecodeComponent */]
+        ],
+        imports: [],
+        providers: []
+    })
+], UrlEncoDedecodeModule);
+
+//# sourceMappingURL=urlencodedecode.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/urlencodedecode/urlencodedecode.style.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "textarea[name=url] {\r\n    width: 100%;\r\n    height: 400px;\r\n    margin-top: 20px;\r\n}\r\n\r\ntextarea {\r\n    width: 100%;\r\n    height: 200px;\r\n    resize: none;\r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/urlencodedecode/urlencodedecode.template.html":
+/***/ (function(module, exports) {
+
+module.exports = "<form>\r\n\t<div class=\"row\">\r\n\t\t<div class=\"col-md-6\">\r\n\t\t\t<div class=\"row\">\r\n\t\t\t\t<div class=\"col-md-12\">\r\n\t\t\t\t\t<div class=\"form-group\">\r\n\t\t\t\t\t\t<textarea name=\"url\" (keyup)=\"onTyping($event)\" [value]=\"model.url\">{{model.url}}</textarea>\r\n\t\t\t\t\t</div>\r\n\t\t\t\t</div>\r\n\t\t\t</div>\r\n\t\t\t<div class=\"row\">\r\n\t\t\t\t<div class=\"col-md-12\">\r\n                    <button class=\"btn btn-default\" (click)=\"clear()\" type=\"button\">Clear</button>\r\n                    <button class=\"btn btn-default\" (click)=\"reset()\" type=\"button\">Reset</button>\r\n\t\t\t\t\t<div class=\"pull-right\">\r\n\t\t\t\t\t\t<button class=\"btn btn-primary\" (click)=\"encodeUrl()\" type=\"button\">Encode</button>\r\n\t\t\t\t\t\t<button class=\"btn btn-primary\" (click)=\"decodeUrl()\" type=\"button\">Decode</button>\r\n\t\t\t\t\t</div>\r\n\t\t\t\t</div>\r\n\t\t\t</div>\r\n\r\n\t\t</div>\r\n\t\t<div class=\"col-md-6\">\r\n\t\t\t<div class=\"row\">\r\n\t\t\t\t<div class=\"col-md-12\">\r\n\t\t\t\t\tInput URL was\r\n\t\t\t\t</div>\r\n\t\t\t</div>\r\n\t\t\t<div class=\"row\">\r\n\t\t\t\t<div class=\"col-md-12\">\r\n\t\t\t\t\t<textarea class=\"original-url\" [value]=\"model.orginalUrl\" readonly></textarea>\r\n\t\t\t\t</div>\r\n\t\t\t</div>\r\n\t\t</div>\r\n\t</div>\r\n</form>\r\n<hr>\r\n<div class=\"row\">\r\n\t<div class=\"col-md-12\">\r\n\t\t<ul>\r\n\t\t\t<li>Paste your URL in above textarea and click on Encode/Decode button.</li>\r\n            <li>textarea in right side contains your original URL.</li>\r\n            <li>Reset button will reset your current session and copy your original url to workspace textarea.</li>\r\n            <li>Clear button will clear your working session including your original URL.</li>\r\n\t\t</ul>\r\n\t</div>\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
